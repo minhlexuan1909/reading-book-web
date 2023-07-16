3a8f2bee20956aebba0313bd7e04934a7e811e3e
minhlexuan1909 <minhlx1909@gmail.com>
2023-07-16 09:58:53 +0700

    last

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 import { createContext, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import {
-  Route,
   BrowserRouter,
   Redirect,
+  Route,
 } from "react-router-dom/cjs/react-router-dom.min";
 
 import { User } from "./components/User";
@@ -32,62 +32,62 @@ function App() {
   const [isLoaded, setIsLoad] = useState(null);
   const [favourite, setFavourite] = useState(null);
   const [keySearch, setKeySearch] = useState("");
-  useEffect(() => {
-    async function fetchAPI() {
-      let resp = await fetch("http://127.0.0.1:8000/book", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token.token}`,
-        },
-      });
-      resp = await resp.json();
-      if (!resp["detail"]) {
-        setBookList(resp);
-      }
-    }
-    fetchAPI();
-  }, [token.token]);
+  // useEffect(() => {
+  //   async function fetchAPI() {
+  //     let resp = await fetch("http://127.0.0.1:8000/book", {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token.token}`,
+  //       },
+  //     });
+  //     resp = await resp.json();
+  //     if (!resp["detail"]) {
+  //       setBookList(resp);
+  //     }
+  //   }
+  //   fetchAPI();
+  // }, [token.token]);
 
-  useEffect(() => {
-    if (userFullname) {
-      const tmp = userFullname.split(" ");
-      setUserLastname(tmp[tmp.length - 1]);
-    }
-  }, [userFullname]);
+  // useEffect(() => {
+  //   if (userFullname) {
+  //     const tmp = userFullname.split(" ");
+  //     setUserLastname(tmp[tmp.length - 1]);
+  //   }
+  // }, [userFullname]);
 
-  useEffect(() => {
-    async function fetchAPI() {
-      let resp = await fetch(`http://127.0.0.1:8000/favourite/${idUser}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token.token}`,
-        },
-      });
-      resp = await resp.json();
-      setFavourite(resp);
-    }
-    if (idUser !== null) fetchAPI();
-  }, [idUser]);
+  // useEffect(() => {
+  //   async function fetchAPI() {
+  //     let resp = await fetch(`http://127.0.0.1:8000/favourite/${idUser}`, {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token.token}`,
+  //       },
+  //     });
+  //     resp = await resp.json();
+  //     setFavourite(resp);
+  //   }
+  //   if (idUser !== null) fetchAPI();
+  // }, [idUser]);
 
-  useEffect(() => {
-    async function fetchAPI() {
-      let resp = await fetch("http://127.0.0.1:8000/isAdmin", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token.token}`,
-        },
-      });
-      resp = await resp.json();
-      if (!resp.detail) {
-        setIsAdmin(true);
-      }
-      setIsLoad(true);
-    }
-    fetchAPI();
-  }, [token.token]);
+  // useEffect(() => {
+  //   async function fetchAPI() {
+  //     let resp = await fetch("http://127.0.0.1:8000/isAdmin", {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token.token}`,
+  //       },
+  //     });
+  //     resp = await resp.json();
+  //     if (!resp.detail) {
+  //       setIsAdmin(true);
+  //     }
+  //     setIsLoad(true);
+  //   }
+  //   fetchAPI();
+  // }, [token.token]);
   const providerValue = {
     isSeeingDetail,
     setIsSeeingDetail,
@@ -110,61 +110,93 @@ function App() {
     keySearch,
     setKeySearch,
   };
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const numberOnChangeHandler = (e) => {
+    const number = e.target.value;
+    const numReg = /([0-9]*[.])?[0-9]+/;
+    if (number.match(numReg)) {
+      setEnteredNumber(parseFloat(number));
+    }
+    // setEnteredNumber(parseFloat(number));
+  };
+  const numberOnKeyPressHandler = (e) => {
+    const number = e.target.value;
+    const numReg = /^[0-9.,]+$/;
+    if (!numReg.test(e.key)) {
+      e.preventDefault();
+    }
+  };
   return (
-    <BookContext.Provider value={providerValue}>
-      <BrowserRouter>
-        <Route exact path="/" component={HomeRoute} />
+    <>
+      <input
+        value={enteredNumber}
+        type="number"
+        onKeyPress={numberOnKeyPressHandler}
+        onChange={numberOnChangeHandler}
+      />
+      <button
+        onClick={() => {
+          console.log(enteredNumber);
+        }}
+      >
+        Click
+      </button>
+    </>
+    // <BookContext.Provider value={providerValue}>
+    //   <BrowserRouter>
+    //   div
+    //     <Route exact path="/" component={HomeRoute} />
 
-        {/* {token["token"] ? (
-          <>
-            <Route exact path="/library" component={BookLibraryRoute} />
-            <Route exact path="/library/:id" component={BookDetailRoute} />
-            <Route exact path="/type/:type" component={BookTypeRoute} />
-            <Route exact path="/type/:type/:id" component={BookDetailRoute} />
-            <Route exact path="/read/:id" component={BookReaderRoute} />
-          </>
-        ) : (
-          // <Redirect to="/login" />
-        )} */}
-        <Route exact path="/library">
-          {token["token"] ? <BookLibraryRoute /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/library/:id">
-          {token["token"] ? <BookDetailRoute /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/type/:type">
-          {token["token"] ? <BookTypeRoute /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/type/:type/:id">
-          {token["token"] ? <BookDetailRoute /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/read/:id">
-          {token["token"] ? <BookReaderRoute /> : <Redirect to="/login" />}
-        </Route>
-        {isLoaded ? (
-          <Route exact path="/favourite">
-            {token["token"] ? <BookFavouriteRoute /> : <Redirect to="/login" />}
-          </Route>
-        ) : (
-          ""
-        )}
-        <Route exact path="/upload" component={BookUploadRoute}>
-          {isLoaded ? (
-            isAdmin ? (
-              <BookUploadRoute />
-            ) : (
-              <Redirect to="/library" />
-            )
-          ) : (
-            ""
-          )}
-        </Route>
-        <Route exact path="/login">
-          {token["token"] ? <Redirect to="/library" /> : <User />}
-        </Route>
-        <Route exact path="/signup" component={User} />
-      </BrowserRouter>
-    </BookContext.Provider>
+    //     {/* {token["token"] ? (
+    //       <>
+    //         <Route exact path="/library" component={BookLibraryRoute} />
+    //         <Route exact path="/library/:id" component={BookDetailRoute} />
+    //         <Route exact path="/type/:type" component={BookTypeRoute} />
+    //         <Route exact path="/type/:type/:id" component={BookDetailRoute} />
+    //         <Route exact path="/read/:id" component={BookReaderRoute} />
+    //       </>
+    //     ) : (
+    //       // <Redirect to="/login" />
+    //     )} */}
+    //     <Route exact path="/library">
+    //       {token["token"] ? <BookLibraryRoute /> : <Redirect to="/login" />}
+    //     </Route>
+    //     <Route exact path="/library/:id">
+    //       {token["token"] ? <BookDetailRoute /> : <Redirect to="/login" />}
+    //     </Route>
+    //     <Route exact path="/type/:type">
+    //       {token["token"] ? <BookTypeRoute /> : <Redirect to="/login" />}
+    //     </Route>
+    //     <Route exact path="/type/:type/:id">
+    //       {token["token"] ? <BookDetailRoute /> : <Redirect to="/login" />}
+    //     </Route>
+    //     <Route exact path="/read/:id">
+    //       {token["token"] ? <BookReaderRoute /> : <Redirect to="/login" />}
+    //     </Route>
+    //     {isLoaded ? (
+    //       <Route exact path="/favourite">
+    //         {token["token"] ? <BookFavouriteRoute /> : <Redirect to="/login" />}
+    //       </Route>
+    //     ) : (
+    //       ""
+    //     )}
+    //     <Route exact path="/upload" component={BookUploadRoute}>
+    //       {isLoaded ? (
+    //         isAdmin ? (
+    //           <BookUploadRoute />
+    //         ) : (
+    //           <Redirect to="/library" />
+    //         )
+    //       ) : (
+    //         ""
+    //       )}
+    //     </Route>
+    //     <Route exact path="/login">
+    //       {token["token"] ? <Redirect to="/library" /> : <User />}
+    //     </Route>
+    //     <Route exact path="/signup" component={User} />
+    //   </BrowserRouter>
+    // </BookContext.Provider>
   );
 }
 
